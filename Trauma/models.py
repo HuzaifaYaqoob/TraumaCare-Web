39b767293b830b718df8fd64aeebee09c3de42cc
RedExpo <redexpo@MacBooks-MacBook-Pro.local>
2023-02-12 21:18:01 +0500

    creating category model

--- Trauma/models.py
+++ Trauma/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+
+from uuid import uuid4
+
+class Category(models.Model):
+    id = models.UUIDField(default=uuid4, primary_key=True, unique=True, editable=False)
