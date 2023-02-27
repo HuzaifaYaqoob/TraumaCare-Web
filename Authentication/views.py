5ce89513d0ced7275b34f305cde501b8baf27270
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-02-27 22:40:38 +0500

    country displayed

--- Authentication/views.py
+++ Authentication/views.py
@@ -3,10 +3,18 @@ from django.shortcuts import render
 # Create your views here.
 
 
+from Trauma.models import Country
+
 
 def LoginPage(request):
     return render(request, 'Auth/login.html')
 
 
 def RegisterPage(request):
-    return render(request, 'Auth/joinpage2.html')
+    context = {
+        'countries' : Country.objects.filter(
+            is_deleted = False,
+            is_active = True
+        )
+    }
+    return render(request, 'Auth/joinpage2.html', context)
