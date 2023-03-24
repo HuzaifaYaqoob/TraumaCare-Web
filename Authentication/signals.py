a40eb588308ea0acf133e33245533ce847e29df0
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-03-24 20:12:02 +0500

    profile creation done while user created

--- Authentication/signals.py
+++ Authentication/signals.py
@@ -6,15 +6,27 @@ from django.dispatch import receiver
 
 
 from Authentication.models import User
+from Profile.models import Profile
 
 @receiver(post_save, sender=User)
 def user_created_signal__CreateUserProfile(sender, instance, created, **kwargs):
     if not created:
-        return
+
+
+        user_name = instance.username
+        first_name, *last_name = user_name.split(' ')
+
+        if len(last_name) > 0:
+            last_name = last_name[0]
+        else:
+            last_name = ''
+
+        user_profile = Profile(
+            user = instance,
+            first_name = first_name,
+            last_name = last_name,
+            full_name = instance.username,
+            email = instance.email,
+        )
+        user_profile.save()
     
-    
-    print('*****************')
-    print('Create user signal callled user_created_signal__CreateUserProfile')
-    print('created : ', created)
-    print('instance : ', instance)
-    print('*****************')
