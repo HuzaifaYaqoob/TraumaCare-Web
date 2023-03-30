f38ef9b0d509d9fe142fd1a6f157b93771633fc3
Huzaifa Yaqoob <63970159+HuzaifaYaqoob@users.noreply.github.com>
2023-03-30 12:22:22 +0500

    Update urls.py

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -2,12 +2,12 @@
 from django.urls import path
 
 
-from .views import LoginPage, RegisterPage, HandleJoin, HandleLogin, HandleLogout
+from .views import LoginPage, RegisterPage, HandleJoin, HandleLogin, HandleLogout, OtpVerificationPage
 
 urlpatterns = [
     path('login/', LoginPage, name='LoginPage'),
     path('join/', RegisterPage, name='RegisterPage'),
-    path('verification/otp/', RegisterPage, name='RegisterPage'),
+    path('verification/otp/', OtpVerificationPage, name='OtpVerificationPage'),
 
     path('logout-handler/', HandleLogout, name='HandleLogout'),
     path('login-handler/', HandleLogin, name='HandleLogin'),
