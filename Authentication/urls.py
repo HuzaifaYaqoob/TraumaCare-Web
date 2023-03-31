bbdb385eeb94ca0221e7359f71d30793b44c1e79
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-03-31 23:03:18 +0500

    user otp verificaation process completed with login and register also completed

--- Authentication/urls.py
+++ Authentication/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path
 
 
-from .views import LoginPage, RegisterPage, HandleJoin, HandleLogin, HandleLogout, OtpVerificationPage
+from .views import LoginPage, RegisterPage, HandleJoin, HandleLogin, HandleLogout, OtpVerificationPage, handleOtp
 
 urlpatterns = [
     path('login/', LoginPage, name='LoginPage'),
@@ -12,4 +12,5 @@ urlpatterns = [
     path('logout-handler/', HandleLogout, name='HandleLogout'),
     path('login-handler/', HandleLogin, name='HandleLogin'),
     path('join-handler/', HandleJoin, name='HandleJoin'),
+    path('otp-handler/', handleOtp, name='handleOtp'),
 ] 
