7b8b7241dcbba69dd7dcff82e9e9d27be10c50a4
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-06-06 06:47:42 +0500

    signup page api

--- Authentication/APIs/v1/urls.py
+++ Authentication/APIs/v1/urls.py
@@ -5,5 +5,8 @@ from django.urls import path
 from Authentication.APIs.v1 import views as v1_apis
 
 urlpatterns = [
-    path('validate-unique-user/', v1_apis.vaidate_unique_user, name='vaidate_unique_user' )
+    path('validate-unique-user/', v1_apis.vaidate_unique_user, name='vaidate_unique_user' ),
+
+    path('login/', v1_apis.Login ),
+    path('signup/', v1_apis.Signup ),
 ] 
