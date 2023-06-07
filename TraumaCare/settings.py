7d2577ad7a628ec99050377635b680b462b5ed4d
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-06-07 06:54:55 +0500

    email sending credentials updated

--- TraumaCare/settings.py
+++ TraumaCare/settings.py
@@ -186,8 +186,8 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "redexpo.official@gmail.com"
-EMAIL_HOST_PASSWORD = "reodpqohzwfzjksq"
+EMAIL_HOST_USER = "traumacare.pk@gmail.com"
+EMAIL_HOST_PASSWORD = "zpsjmfoaipkndfqy"
 
 # CONSTANT VARIABLES 
 
