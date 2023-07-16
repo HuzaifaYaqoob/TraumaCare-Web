5f1419ecf6865e983074cc0a66a2b6e6eaf55c73
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-07-16 10:28:02 +0500

    asgi file conf

--- TraumaCare/asgi.py
+++ TraumaCare/asgi.py
@@ -9,20 +9,22 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
+# from channels.auth import AuthMiddlewareStack
+# from channels.routing import ProtocolTypeRouter, URLRouter
+# from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
 import django
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'TraumaCare.settings')
 django.setup()
 
-import ChatXpo.Sockets.Routing.MainRouting
+# import ChatXpo.Sockets.Routing.MainRouting
 
-application = ProtocolTypeRouter({
-    'http' : get_asgi_application(),
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(URLRouter(ChatXpo.Sockets.Routing.MainRouting.websocket_urlpatterns))
-    ),
-})
+application = get_asgi_application()
+
+# application = ProtocolTypeRouter({
+#     'http' : get_asgi_application(),
+#     "websocket": AllowedHostsOriginValidator(
+#         AuthMiddlewareStack(URLRouter(ChatXpo.Sockets.Routing.MainRouting.websocket_urlpatterns))
+#     ),
+# })
