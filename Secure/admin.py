5f1e012165cd73edf1c00db0b146a901c6729c06
Huzaifa Yaqoob <63970159+HuzaifaYaqoob@users.noreply.github.com>
2023-07-18 12:08:18 +0500

    Update admin.py

--- Secure/admin.py
+++ Secure/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from Secure.models import XpoKey
+from Secure.models import XpoKey, ApplicationReview
 # Register your models here.
 
 
 admin.site.register(XpoKey)
+
+@admin.register(ApplicationReview)
+class ApplicationReviewAdmin(admin.ModelAdmin):
+  list_display = ['id', 'name', 'email', 'rating', 'created_at' ]
