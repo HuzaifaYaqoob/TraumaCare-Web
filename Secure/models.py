0d8ea3a31d184678235ddc148e5ff74ef11ac942
Huzaifa Yaqoob <63970159+HuzaifaYaqoob@users.noreply.github.com>
2023-07-18 12:02:52 +0500

    Created application review table Update models.py

--- Secure/models.py
+++ Secure/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.utils.timezone import now
+from Authentication.models import User
 # Create your models here.
 
 
@@ -8,3 +9,20 @@ class XpoKey(models.Model):
 
     def __str__(self):
         return f'{self.id}'
+
+class ApplicationReview(models.Model):
+    uuid = models.CharField(default=uuid.uuid4, editable=False, max_length=999)
+
+    user = models.ForeignKey(User, on_delete=models.PROTECT, null=True, blank=True, related_name='user_application_reviews')
+    name = models.CharField(max_length=225, default='')
+    email = models.CharField(max_length=225, default='')
+
+    rating = models.FloatField(default=0)
+    text = models.TextField(default='')
+
+    is_deleted = models.BooleanField(default=False)
+    is_blocked = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=now)
+
+    def __str__(self):
+        return f'{self.uuid}'
