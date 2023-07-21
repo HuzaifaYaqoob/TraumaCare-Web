44db480c841d0ef940f02b4b42533aac49002e7f
HuzaifaYaqoob <zeevhyorg@gmail.com>
2023-07-21 22:58:33 +0500

    db setuped

--- TraumaCare/settings.py
+++ TraumaCare/settings.py
@@ -125,23 +125,23 @@ CHANNEL_LAYERS = {
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': 'traumacaredb',
-#         'USER': 'huzaifa',
-#         'PASSWORD': 'mypassword',
-#         'HOST': 'traumacare-main.cfdpz1rhie5j.eu-north-1.rds.amazonaws.com',
-#         'PORT': 5432,
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'traumacaredb',
+        'USER': 'huzaifa',
+        'PASSWORD': 'mypassword',
+        'HOST': 'traumacare-main.cfdpz1rhie5j.eu-north-1.rds.amazonaws.com',
+        'PORT': 5432,
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
