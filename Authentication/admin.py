087b102a406b31d3553509a45ae6bbd8745ceefa
Huzaifa Yaqoob <63970159+HuzaifaYaqoob@users.noreply.github.com>
2023-08-02 11:57:37 +0500

    Update admin.py

--- Authentication/admin.py
+++ Authentication/admin.py
@@ -9,6 +9,6 @@ from .models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     ordering = ['-joined_at']
-    list_display = ['id', 'username', 'email', 'country_name']
+    list_display = ['id', 'username', 'email', 'country_name', 'joined_at']
 
     search_fields = ['id', 'username', 'email', 'country__name', 'joined_at']
