05b5177a3d6a69d41ad6c17996ef2cdf4d676da7
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-06-04 19:46:30 +0500

    hospital profile customization

--- Hospital/urls.py
+++ Hospital/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 from . import views as hospital_views
 
 urlpatterns = [
-    path('search/', hospital_views.HospitalSearchPage, name='HospitalSearchPage')
+    path('search/', hospital_views.HospitalSearchPage, name='HospitalSearchPage'),
+    path('<str:hospital_id>/', hospital_views.ViewHospitalProfile, name='ViewHospitalProfile')
 ] 
--- Hospital/views.py
+++ Hospital/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
 
 # Create your views here.
+from Hospital.models import Hospital
+from Doctor.models import Doctor
+from django.contrib import messages
 
 
 def HospitalSearchPage(request):
     return render(request, '')
+
+def ViewHospitalProfile(request, hospital_id):
+    try:
+        hospital = Hospital.objects.get(id = hospital_id, is_deleted = False, is_active=True, is_blocked = False)
+    except:
+        messages.error(request, 'Invalid Hospital')
+        return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
+    
+    context = {
+        'hospital' : hospital,
+    }
+
+    context['doctors'] = Doctor.objects.filter(doctor_hospital_timeslots__hospital = hospital, is_deleted = False, is_blocked = False, is_active = True)
+
+    return render(request, 'Hospital/hospitalprofle.html', context)
