aaa759e87e15e477fef39b7ea8bb02c20e8b3af4
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-06-04 15:52:47 +0500

    changes

--- Doctor/views.py
+++ Doctor/views.py
@@ -55,22 +55,23 @@ def DoctorProfilePage(request, doctor_id):
         online_availability_data = {}
         
         online_avas = DoctorTimeSlots.objects.filter(doctor = doctor, availability_type = 'Online', is_deleted = False, is_active=True)
-        for online_ava in online_avas:
-            date_data = online_ava.slot_next_date
-            if len(date_data) > 0 and 'day_date' in date_data:
-                online_availability_data[date_data['day_date']] = online_availability_data.get(date_data['day_date']) or {'slots' : [], 'highest_discount' : {'discount' : online_ava.discount, 'day' : online_ava.day_abbr}}
-                online_availability_data[date_data['day_date']].update(date_data)
-                online_availability_data[date_data['day_date']]['slots'].append(online_ava)
+        if len(online_avas) > 0 :
+            for online_ava in online_avas:
+                date_data = online_ava.slot_next_date
+                if len(date_data) > 0 and 'day_date' in date_data:
+                    online_availability_data[date_data['day_date']] = online_availability_data.get(date_data['day_date']) or {'slots' : [], 'highest_discount' : {'discount' : online_ava.discount, 'day' : online_ava.day_abbr}}
+                    online_availability_data[date_data['day_date']].update(date_data)
+                    online_availability_data[date_data['day_date']]['slots'].append(online_ava)
 
-                if online_ava.discount > 0 and online_ava.discount > online_availability_data[date_data['day_date']]['highest_discount']['discount']:
-                    dics = {'discount' : online_ava.discount, 'day' : online_ava.day_abbr}
-                    online_availability_data[date_data['day_date']]['highest_discount'] = dics
-        
-        data = sorted(online_availability_data.items(), key = lambda x:datetime.strptime(x[0], '%Y-%m-%d'))
-        print(type(data))
-        online_availability_data = {data[0][0] : data[0][1]}
+                    if online_ava.discount > 0 and online_ava.discount > online_availability_data[date_data['day_date']]['highest_discount']['discount']:
+                        dics = {'discount' : online_ava.discount, 'day' : online_ava.day_abbr}
+                        online_availability_data[date_data['day_date']]['highest_discount'] = dics
+            
+            data = sorted(online_availability_data.items(), key = lambda x:datetime.strptime(x[0], '%Y-%m-%d'))
+            print(type(data))
+            online_availability_data = {data[0][0] : data[0][1]}
 
-        context['online_availability'] = online_availability_data
+            context['online_availability'] = online_availability_data
 
 
 
