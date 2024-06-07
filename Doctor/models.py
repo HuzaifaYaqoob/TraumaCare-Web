d2a9504ff923aa8fb2908e117dae2d343bd8a71e
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-06-07 18:18:40 +0500

    changes

--- Doctor/models.py
+++ Doctor/models.py
@@ -148,6 +148,7 @@ class Doctor(models.Model):
     @property
     def is_available_today(self):
         today = datetime.now()
+        return today.time()
         slots = DoctorTimeSlots.objects.filter(
             doctor = self,
             day__day = today.strftime('%A'),
