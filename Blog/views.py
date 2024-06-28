4405f037fb3cc21085668b46b43337c991aa4d63
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-06-29 00:15:47 +0500

    images

--- Blog/views.py
+++ Blog/views.py
@@ -2,11 +2,12 @@ from django.shortcuts import render
 
 from Blog.models import BlogPost
 
+from django.db.models import Count
 # Create your views here.
 
 
 def BlogHomePage(request):
     context = {
-        'posts' : BlogPost.objects.all().order_by('-created_at')[:20]
+        'posts' : BlogPost.objects.annotate(media = Count('media')).filter(media__gt = 0).order_by('-created_at')[:20]
     }
     return render(request, 'Blog/blog-home.html', context)
