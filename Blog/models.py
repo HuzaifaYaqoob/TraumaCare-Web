1976197e58e90e1b53a15ea6068d69528ff0d5d2
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-06-29 16:22:50 +0500

    images

--- Blog/models.py
+++ Blog/models.py
@@ -145,7 +145,7 @@ class BlogMedia(models.Model):
             
             slug = self.post.slug
             slug = slug.replace(' ', '-').replace('/', '-').replace(':', '-').replace('--', '-')
-            saving_url = f"media/Blog/Images/{slug[0:40]}{time_now.strftime("%d-%H%M%S")}.jpeg"
+            saving_url = f"media/Blog/Images/{slug[0:44]}-{time_now.strftime("%d-%H%M%S")}.jpeg"
             background.save(saving_url, quality=70)
             self.image = f'{saving_url}'.split('media/')[-1]
             self.is_thumbnail_generated = True
