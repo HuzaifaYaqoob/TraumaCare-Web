0b3ba94eb8022e0ca1c271c7d61dc3a0cfd00bfa
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-08-17 14:22:25 +0500

    removing bulk messages

--- Appointment/signals.py
+++ Appointment/signals.py
@@ -10,7 +10,6 @@ from Administration.models import PhoneMessage
 @receiver(post_save, sender=Appointment)
 def generatePhoneMessage_Appointment(sender, instance, created, **kwargs):
     if not instance.is_sms_sent:
-        print()
 
         if type(instance.date) == str:
             app_date = instance.date
@@ -33,28 +32,19 @@ def generatePhoneMessage_Appointment(sender, instance, created, **kwargs):
                 sms_type = 'OnlineAppointment',
                 text = f"You have a new online appointment with {instance.appointment_group.user.full_name} on {app_date} at {app_time}, booked via TraumaCare, powered by RedExpo."
             )
-            instance.is_sms_sent = True
-            instance.save()
+
         elif instance.appointment_location == 'InPerson' and instance.doct_hospital:
             hospital = instance.doct_hospital.hospital
             location = instance.doct_hospital.location
-            # Send Message to Patient
+            patient = instance.appointment_group.user
+            doctor = instance.doctor
+
+            to = f'{patient.mobile_number},{doctor.user.mobile_number},{instance.doct_hospital.phone}'
             PhoneMessage.objects.create(
-                phone_number = instance.appointment_group.user.mobile_number,
+                phone_number = to,
                 sms_type = 'Appointment',
-                text = f"Your appointment with Dr. {instance.doctor.name} at {hospital.name}, {location.name} on {app_date} at {app_time} has been confirmed by TraumaCare, powered by RedExpo."
+                text = f"*Appointment Confirmation:* {patient.full_name} has an appointment with Dr. {doctor.name} at {hospital.name}, {location.name}, on {app_date} at {app_time}. Confirmed via TraumaCare, powered by RedExpo."
             )
-            # Send Message to Doctor
-            PhoneMessage.objects.create(
-                phone_number = instance.doctor.user.mobile_number,
-                sms_type = 'Appointment',
-                text = f"You have a new appointment with {instance.appointment_group.user.full_name} at {hospital.name}, {location.name} on {app_date} at {app_time}, booked via TraumaCare, powered by RedExpo."
-            )
-            # Send Message to Hospital
-            PhoneMessage.objects.create(
-                phone_number = instance.doct_hospital.phone,
-                sms_type = 'Appointment',
-                text = f"An appointment has been scheduled with Dr. {instance.doctor.name} on {app_date} at {app_time}. Patient: {instance.appointment_group.user.full_name}. Location: {hospital.name}, {location.name}. Confirmed via TraumaCare, powered by RedExpo."
-            )
-            instance.is_sms_sent = True
-            instance.save()
+
+        instance.is_sms_sent = True
+        instance.save()
