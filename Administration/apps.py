3bbc1b091b14b7234c20f2a8bd435d8015005e49
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-08-17 13:25:25 +0500

    send sms to phone number

--- Administration/apps.py
+++ Administration/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class AdministrationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'Administration'
+
+    def ready(self):
+        import Administration.signals
