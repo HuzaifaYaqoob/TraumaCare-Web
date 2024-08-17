d48a7f84ee6a59e45af865f72ed0a91dd0b13946
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-08-17 22:27:20 +0500

    changes

--- Authentication/APIs/device/views.py
+++ Authentication/APIs/device/views.py
@@ -64,78 +64,85 @@ def HandleOtpVerification(request):
 @permission_classes([AllowAny])
 def HandleLogin(request):
     
-    mobile_number = request.data.get('mobile_number', None)
-    
+    try:
+        mobile_number = request.data.get('mobile_number', None)
+        
 
-    if not mobile_number or len(mobile_number) != 11 or not mobile_number.startswith('03'):
+        if not mobile_number or len(mobile_number) != 11 or not mobile_number.startswith('03'):
+            return Response({
+                'status' : False,
+                'message' : 'Invalid Phone Number',
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        try:
+            user = User.objects.get(mobile_number = mobile_number)
+        except:
+            full_name = request.data.get('full_name', None)
+            if not full_name:
+                return Response({
+                    'status' : False,
+                    'message' : 'Full name required',
+                }, status=status.HTTP_400_BAD_REQUEST)
+
+            username = full_name.replace(' ', '')
+            keepChecking = True
+            while keepChecking:
+                try:
+                    user = User.objects.get(username = username)
+                except:
+                    keepChecking = False
+                else:
+                    username = f'{username}-{random.randint(1000, 9999)}'
+
+            email = f'{username}-{mobile_number}@traumacare.pk'
+            password = f'{username}-{mobile_number}'
+            dial_code = '92'
+            full_name = f'{full_name} '.split(' ')
+            first_name = full_name[0]
+            try:
+                last_name = full_name[1]
+            except:
+                last_name = ''
+
+            user = User(
+                username = username,
+                email = email,
+                first_name = first_name,
+                last_name = last_name,
+                dial_code = dial_code,
+                mobile_number = mobile_number
+            )
+            user.set_password(password)
+            user.save()
+
+            VerificationCode.objects.create(
+                user = user,
+                otp_type = 'MOBILE_VERIFICATION'
+            )
+
+            return Response({
+                'status' : True,
+                'message' : 'OTP Sent Successfully',
+            })
+        else:
+            # Login User here.
+            if user.is_deleted:
+                return Response({
+                    'status' : False,
+                    'message' : 'You are not allowed to login with this Phone Number.',
+                }, status=status.HTTP_400_BAD_REQUEST)
+
+            VerificationCode.objects.create(
+                user = user,
+                otp_type = 'MOBILE_VERIFICATION'
+            )
+            return Response({
+                'status' : True,
+                'message' : 'OTP Sent Successfully',
+            })
+
+    except Exception as err:
         return Response({
             'status' : False,
-            'message' : 'Invalid Phone Number',
+            'message' : str(err),
         }, status=status.HTTP_400_BAD_REQUEST)
-
-    try:
-        user = User.objects.get(mobile_number = mobile_number)
-    except:
-        full_name = request.data.get('full_name', None)
-        if not full_name:
-            return Response({
-                'status' : False,
-                'message' : 'Full name required',
-            }, status=status.HTTP_400_BAD_REQUEST)
-
-        username = full_name.replace(' ', '')
-        keepChecking = True
-        while keepChecking:
-            try:
-                user = User.objects.get(username = username)
-            except:
-                keepChecking = False
-            else:
-                username = f'{username}-{random.randint(1000, 9999)}'
-
-        email = f'{username}-{mobile_number}@traumacare.pk'
-        password = f'{username}-{mobile_number}'
-        dial_code = '92'
-        full_name = f'{full_name} '.split(' ')
-        first_name = full_name[0]
-        try:
-            last_name = full_name[1]
-        except:
-            last_name = ''
-
-        user = User(
-            username = username,
-            email = email,
-            first_name = first_name,
-            last_name = last_name,
-            dial_code = dial_code,
-            mobile_number = mobile_number
-        )
-        user.set_password(password)
-        user.save()
-
-        VerificationCode.objects.create(
-            user = user,
-            otp_type = 'MOBILE_VERIFICATION'
-        )
-
-        return Response({
-            'status' : True,
-            'message' : 'OTP Sent Successfully',
-        })
-    else:
-        # Login User here.
-        if user.is_deleted:
-            return Response({
-                'status' : False,
-                'message' : 'You are not allowed to login with this Phone Number.',
-            }, status=status.HTTP_400_BAD_REQUEST)
-
-        VerificationCode.objects.create(
-            user = user,
-            otp_type = 'MOBILE_VERIFICATION'
-        )
-        return Response({
-            'status' : True,
-            'message' : 'OTP Sent Successfully',
-        })
