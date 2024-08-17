df7743c53c76438f1661946e198abbbf2d704598
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-08-17 14:27:31 +0500

    removing bulk messages

--- Appointment/signals.py
+++ Appointment/signals.py
@@ -18,26 +18,20 @@ def generatePhoneMessage_Appointment(sender, instance, created, **kwargs):
             app_date = instance.date.strftime('%B %d, %Y')
             app_time = instance.start_time.strftime('%I:%M %p')
 
-        if instance.appointment_location == 'Online':
+        patient = instance.appointment_group.user
+        doctor = instance.doctor
 
-            # Send Message to Patient
+        if instance.appointment_location == 'Online':
+            to = f"{patient.mobile_number},{doctor.user.mobile_number}"
             PhoneMessage.objects.create(
-                phone_number = instance.appointment_group.user.mobile_number,
+                phone_number = to,
                 sms_type = 'OnlineAppointment',
-                text = f"Your online appointment with Dr. {instance.doctor.name} on {app_date} at {app_time} has been confirmed by TraumaCare, powered by RedExpo."
-            )
-            # Send Message to Doctor
-            PhoneMessage.objects.create(
-                phone_number = instance.doctor.user.mobile_number,
-                sms_type = 'OnlineAppointment',
-                text = f"You have a new online appointment with {instance.appointment_group.user.full_name} on {app_date} at {app_time}, booked via TraumaCare, powered by RedExpo."
+                text = f"*Online appointment confirmed:* {patient.full_name} has an online appointment with Dr. {doctor.name} on {app_date} at {app_time}, booked via TraumaCare, powered by RedExpo."
             )
 
         elif instance.appointment_location == 'InPerson' and instance.doct_hospital:
             hospital = instance.doct_hospital.hospital
             location = instance.doct_hospital.location
-            patient = instance.appointment_group.user
-            doctor = instance.doctor
 
             to = f'{patient.mobile_number},{doctor.user.mobile_number},{instance.doct_hospital.phone}'
             PhoneMessage.objects.create(
