ff8dac7be21c176ac797bc0fa23d72c7072612c6
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-08-22 14:37:32 +0500

    hospitals

--- Doctor/APIs/v2/serializers.py
+++ Doctor/APIs/v2/serializers.py
@@ -3,7 +3,7 @@
 
 from rest_framework import serializers
 from django.conf import settings
-from Doctor.models import Doctor
+from Doctor.models import Doctor, DoctorWithHospital
 from datetime import datetime, timedelta
 
 class DeviceHomePageDoctorsSerializer(serializers.ModelSerializer):
@@ -28,10 +28,27 @@ class DeviceHomePageDoctorsSerializer(serializers.ModelSerializer):
             'img',
         ]
 
+
+class DoctorWithHospitalViewProfileSerializer(serializers.ModelSerializer):
+    name = serializers.CharField(source='hospital.name')
+    address = serializers.CharField(source='location.name')
+
+    class Meta:
+        model = DoctorWithHospital  
+        fields = [
+            'id',
+            'name',
+            'address',
+        ]
+
 class DoctorSingleProfileGet(serializers.ModelSerializer):
     sp = serializers.SerializerMethodField()
     img = serializers.SerializerMethodField()
     days = serializers.SerializerMethodField()
+    hospitals = serializers.SerializerMethodField()
+
+    def get_hospitals(self, doctor):
+        return DoctorWithHospitalViewProfileSerializer(DoctorWithHospital.objects.filter(doctor = doctor, is_deleted = False, is_active = True), many=True).data
 
     def get_days(self, doctor):
         days_slots = []
