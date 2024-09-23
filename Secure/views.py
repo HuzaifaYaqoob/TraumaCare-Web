0e3fe0bfd299a67911cef138d6a8f075ab5065fa
HuzaifaYaqoob <zeevhyorg@gmail.com>
2024-09-23 08:28:45 +0500

    changes

--- Secure/views.py
+++ Secure/views.py
@@ -7,10 +7,10 @@ from Secure.models import ApplicationReview
 
 
 def handleApplicationReviewSubmit(request):
-    username = request.POST.get('username', None)
-    mobile_number = request.POST.get('mobile_number', None)
-    star_rating = request.POST.get('star_rating', None)
-    content = request.POST.get('content', None)
+    username = request.POST.get('username', 'No Username')
+    mobile_number = request.POST.get('mobile_number', 'No Phone')
+    star_rating = request.POST.get('star_rating', 1)
+    content = request.POST.get('content', 'No Review')
 
     user = request.user
 
