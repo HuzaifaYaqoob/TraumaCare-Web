0eb24f3755bd0ff55710f8be3d036142e588d4d3
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2024-12-16 19:44:46 +0500

    Auto commit at 2024-12-16 19:44:46

--- Authentication/admin.py
+++ Authentication/admin.py
@@ -73,7 +73,7 @@ class CustomUserAdmin(UserAdmin):
             },
         ),
     )
-    def phone_number(self, user):
+    def phone_number(self, user, request):
         is_mobile_verified = '<img style="margin-right:7px" src="%s" />' % ('https://traumaaicare.com/static/admin/img/icon-yes.svg' if user.is_mobile_verified else 'https://traumaaicare.com/static/admin/img/icon-no.svg')
         return mark_safe(f'{is_mobile_verified} {user.mobile_number}')
     
