2fef85507deb13063252ad3c5e9bc7544f652138
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2024-12-29 18:21:55 +0500

    Auto commit at 2024-12-29 18:21:55

--- Trauma/management/commands/test.py
+++ Trauma/management/commands/test.py
@@ -6,17 +6,11 @@ from django.core.management.base import BaseCommand
 
 import csv
 from django.conf import settings
-from PIL import Image
-
-from ChatXpo.Sockets.Constant.Query import askChatXpo
-
 from datetime import datetime
 import time
-
-from openai import OpenAI
-
 import re
 
+import geopy.distance
 
 class Command(BaseCommand):
     def __init__(self, *args, **kwargs):
@@ -24,38 +18,10 @@ class Command(BaseCommand):
 
 
     def handle(self, *args, **options):
-        file_path = 'Files/WhatsApp Image 2024-08-02 at 16.13.06.jpeg'
+        my_coords = (31.48457979046869, 74.26311376936425)
+        hospital_coords = (31.47969739187253, 74.2803748868337)
 
-        import pytesseract
-        from PIL import Image
-
-        # Open the image file
-        image = Image.open(file_path)
-
-        # Perform OCR using PyTesseract
-        text = pytesseract.image_to_string(image)
-
-        text = re.sub(r'[^\x00-\x7F]+', ' ', text)
-        text = re.sub(r'\s+', ' ', text).strip()
-
-        # Print the extracted text
-        print(text)
-
-
-        # client = OpenAI(api_key='')
-
-        # queries = [ {"role": "system", "content": "You are a helpful assistant that can read images."}, 
-        #            {"role": "user", "content": f"Extract the text from the image from "} ]
-
-        # response = client.chat.completions.create(
-        #     model = 'gpt-4-vision-preview',
-        #     messages = queries,
-        # )
-        # choice = response.choices[0]
-        # chat_message = response.choices[0].message
-        # print(chat_message)
-
-
+        print(geopy.distance.geodesic(my_coords, hospital_coords).km)
 
 
         self.stdout.write(self.style.SUCCESS('Successfully added Specialities'))
