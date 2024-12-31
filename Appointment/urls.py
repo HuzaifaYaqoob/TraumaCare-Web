5ecfe3022004c2c74011962ee43c1009a7cfbbe0
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2024-12-31 20:28:18 +0500

    Auto commit at 2024-12-31 20:28:18

--- Appointment/urls.py
+++ Appointment/urls.py
@@ -11,7 +11,7 @@ from django.contrib.auth.decorators import login_required
 urlpatterns = [
     path('my-appointments', login_required(apptView.MyAppointmentsPage, login_url='/auth/login/'), name='MyAppointmentsPage'),
     path('my-appointments/cancel-appointment/<str:appointment_id>/', login_required(apptView.CancelMyAppointment, login_url='/auth/login/'), name='CancelMyAppointment'),
-    path('book-appointment', login_required(apptView.BookAppointmentPage, login_url='/auth/login/'), name='BookAppointmentPage'),
+    path('book-appointment/', apptView.BookAppointmentPage, name='BookAppointmentPage'),
     path('checkout/', login_required(apptView.CheckoutPage, login_url='/auth/login/'), name='CheckoutPage'),
 
     path('book-instant-appointment', login_required(apptView.BookAppointment_DoctorPage, login_url='/auth/login/'), name='BookAppointment_DoctorPage'),
