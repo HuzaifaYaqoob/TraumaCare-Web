9d38ffbe10ab211b33c1e7c697c0778e60b2015d
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2025-01-01 11:01:32 +0500

    Auto commit at 2025-01-01 11:01:32

--- Pharmacy/admin.py
+++ Pharmacy/admin.py
@@ -17,9 +17,15 @@ class StoreMediaInline(admin.StackedInline):
 @admin.register(Store)
 class StoreAdmin(admin.ModelAdmin):
     inlines = [StoreLocationInline, StoreMediaInline]
+    search_fields = ['name']
     list_display = [
-        'name', 'phone',
+        'store',
         'created_at', 'updated_at', 'is_active', 'is_blocked',
     ]
 
     exclude = ['user', 'profile', 'slug', 'created_at', 'updated_at']
+
+    @admin.display(description='Store')
+    def store(self, obj):
+        return obj.store_admin_card()
+    store.admin_order_field = 'name'
