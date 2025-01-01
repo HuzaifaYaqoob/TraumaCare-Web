8a9a14ce0358196fb5f6bff218ffa3cedebb7d02
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2025-01-01 10:45:10 +0500

    Auto commit at 2025-01-01 10:45:10

--- Pharmacy/admin.py
+++ Pharmacy/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Store, StoreLocation, StoreMedia
+
+
+
+class StoreLocationInline(admin.TabularInline):
+    model = StoreLocation
+    extra = 0
+
+class StoreMediaInline(admin.TabularInline):
+    model = StoreMedia
+    extra = 0
+
+@admin.register(Store)
+class StoreAdmin(admin.ModelAdmin):
+    inlines = [StoreLocationInline, StoreMediaInline]
+    list_display = [
+        'name', 'phone',
+        'created_at', 'updated_at', 'is_active', 'is_blocked',
+    ]
