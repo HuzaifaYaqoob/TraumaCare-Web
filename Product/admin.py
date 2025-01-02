cb643f329cebc8d8ebd425784242e08c30b3c7c0
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2025-01-02 18:35:46 +0500

    Auto commit at 2025-01-02 18:35:46

--- Product/admin.py
+++ Product/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import Product, ProductImage, ProductCategory, SubCategory, ProductStock, ProductForm, ProductType, TreatmentType
+from django.utils.html import mark_safe
 
 
 @admin.register(ProductCategory)
@@ -16,8 +17,23 @@ class SubCategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ['name']
+    search_fields = ['name']
+    list_display = [
+        'name',
+        'price',
+        'product_store',
+        'Vendor',
+        'manufacturer',
+        'treatment_type',
+        'product_form',
+        'product_type',
+        'Images',
+    ]
 
+    @admin.display(description='Store')
+    def product_store(self, obj):
+        return obj.store
+    product_store.admin_order_field = 'store'
 
 @admin.register(ProductImage)
 class ProductImageAdmin(admin.ModelAdmin):
