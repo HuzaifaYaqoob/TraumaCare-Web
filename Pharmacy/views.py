47f3d07d58f92b39198063765766a37bf95ec3e2
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2025-01-04 18:31:23 +0500

    Auto commit at 2025-01-04 18:31:23

--- Pharmacy/views.py
+++ Pharmacy/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 
+def PharmacyLandingPage(request):
+    return render(request, 'Pharmacy/pharmacy_landing.html')
 
 def PharmacySearchPage(request):
     return render(request, 'Pharmacy/pharmacy_search.html')
