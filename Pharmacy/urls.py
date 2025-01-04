6997fd5813ec464c6ccf396c7368b88e1ea44d1e
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2025-01-04 18:51:50 +0500

    Auto commit at 2025-01-04 18:51:50

--- Pharmacy/urls.py
+++ Pharmacy/urls.py
@@ -5,5 +5,6 @@ from django.urls import path
 from . import views as pharmacy_views
 
 urlpatterns = [
-    path('search/', pharmacy_views.PharmacySearchPage, name='PharmacySearchPage')
+    path('', pharmacy_views.PharmacyLandingPage, name='PharmacyLandingPage'),
+    path('search/', pharmacy_views.PharmacySearchPage, name='PharmacySearchPage'),
 ] 
