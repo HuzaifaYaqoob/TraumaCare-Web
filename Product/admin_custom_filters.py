1b003fbe569d76b4a616a425048c027a4dab8bd7
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2025-01-04 17:20:19 +0500

    Auto commit at 2025-01-04 17:20:18

--- Product/admin_custom_filters.py
+++ Product/admin_custom_filters.py
@@ -1,12 +1,22 @@
 from django.contrib.admin import SimpleListFilter
+from django.db.models import Count
 
 class ImageCountFilter(SimpleListFilter):
     title = 'Images Count' # or use _('country') for translated title
     parameter_name = 'Images Count'
 
     def lookups(self, request, model_admin):
-        return [('AFRICA', 'AFRICA - ALL')]
+        return [
+            ('0', 'Zero Images'),
+            ('1', '1 Image'),
+            ('2', '2 Images'),
+            ('3', '3 Images'),
+            ('4', '4 Images'),
+            ('5', '5 Images'),
+            ('6', '6 Images'),
+        ]
 
     def queryset(self, request, queryset):
-        if self.value() == 'AFRICA':
-            return queryset
+        val = self.value()
+        if val:
+            return queryset.annotate(images_count = Count('product_images')).filter(images_count=val)
