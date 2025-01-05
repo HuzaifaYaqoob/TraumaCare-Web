b1c6151135cde3c8cb86108be471407380358be9
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2025-01-05 18:00:39 +0500

    Auto commit at 2025-01-05 18:00:39

--- Pharmacy/urls.py
+++ Pharmacy/urls.py
@@ -7,4 +7,5 @@ from . import views as pharmacy_views
 urlpatterns = [
     path('', pharmacy_views.PharmacyLandingPage, name='PharmacyLandingPage'),
     path('search/', pharmacy_views.PharmacySearchPage, name='PharmacySearchPage'),
+    path('cart/', pharmacy_views.PharmacySearchPage, name='PharmacySearchPage'),
 ] 
