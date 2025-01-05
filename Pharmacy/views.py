38e4c577b938ee8e0f2dadc29e3a359225d8af5a
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2025-01-05 18:17:42 +0500

    Auto commit at 2025-01-05 18:17:42

--- Pharmacy/views.py
+++ Pharmacy/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
+from Product.models import Product
 # Create your views here.
 
 def PharmacyLandingPage(request):
-    return render(request, 'Pharmacy/pharmacy_landing.html')
+    context = {}
+    medicines = Product.objects.filter(is_active=True, is_deleted=False, is_blocked=False)[:10]
+
+
+    context['medicines'] = medicines
+    return render(request, 'Pharmacy/pharmacy_landing.html', context)
 
 def PharmacySearchPage(request):
     return render(request, 'Pharmacy/pharmacy_search.html')
