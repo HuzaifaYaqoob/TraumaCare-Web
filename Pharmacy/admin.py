a790a60de8455f34f83add9471102a0dd796cdcd
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2025-01-05 15:10:51 +0500

    Auto commit at 2025-01-05 15:10:51

--- Pharmacy/admin.py
+++ Pharmacy/admin.py
@@ -20,6 +20,7 @@ class StoreAdmin(admin.ModelAdmin):
     search_fields = ['name']
     list_display = [
         'store',
+        'locations',
         'created_at', 'updated_at', 'is_active', 'is_blocked',
     ]
 
@@ -29,3 +30,6 @@ class StoreAdmin(admin.ModelAdmin):
     def store(self, obj):
         return obj.store_admin_card()
     store.admin_order_field = 'name'
+
+    def locations(self, store):
+        return store.store_locations.count()
