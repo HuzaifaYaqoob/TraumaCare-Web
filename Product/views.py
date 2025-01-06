c52b43d613121bb67e13156c7649adbc2e613d48
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2025-01-06 08:04:03 +0500

    Auto commit at 2025-01-06 08:04:03

--- Product/views.py
+++ Product/views.py
@@ -41,15 +41,11 @@ def SingleMedicineViewPage(request, product_slug):
     for location_stock in other_locations:
         product_all_images = location_stock.product.product_all_images
         other_locations_data.append({
-            'slug' : location_stock.product.slug,
-            'name' : location_stock.product.name,
-            'store_name' : location_stock.product.store.name,
             'location_name' : location_stock.location.name,
             'location_id' : location_stock.location.id,
             'price' : location_stock.price,
             'final_price' : location_stock.final_price,
             'discount' : location_stock.discount,
-            'image' : product_all_images[0].image.url if len(product_all_images) > 0 else None
         })
     context['other_locations'] = other_locations_data
     return render(request, 'Medicine/SingleMedicineViewPage.html', context)
