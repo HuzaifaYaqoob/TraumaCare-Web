02581f18f946c496e94a3c631334b8422b6a99d7
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2025-01-08 16:02:53 +0500

    Auto commit at 2025-01-08 16:02:53

--- Pharmacy/views.py
+++ Pharmacy/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.db.models import Q
-from Product.models import Product
+from Product.models import Product, ProductStock
+import json
+from urllib.parse import unquote
 # Create your views here.
 
 def PharmacyLandingPage(request):
@@ -24,4 +26,58 @@ def PharmacySearchPage(request):
     return render(request, 'Pharmacy/pharmacy_search.html', context)
 
 def PharmacyCartPage(request):
-    return render(request, 'Pharmacy/pharmacy_cart.html')
+    cookie_data = request.COOKIES.get('CartItems', '')
+
+    decoded_data = unquote(cookie_data)
+    # Parse JSON data to Python list
+    CartItems = json.loads(decoded_data)
+    data = []
+    subtotal = 0
+    discount_applied = 0
+    platform_fee = 9
+    delivery_charges = 149
+    grand_total = 0
+
+    for item in CartItems:
+        try:
+            product = Product.objects.get(slug = item['slug'])
+            stock = ProductStock.custom_objects.get(id = item['location_stock'])
+        except Exception as err:
+            print(err)
+            pass
+        else:
+            quantity = int(item['quantity'])
+            images = product.product_all_images
+            image = None
+            if len(images) > 0 and images[0].image:
+                image = images[0].image.url
+            subtotal += stock.final_price * quantity
+            if stock.discount:
+                discount_applied += (stock.price - stock.final_price) * quantity
+
+            data.append({
+                'id' : product.id,
+                'slug' : product.slug,
+                'name' : product.name,
+                'location_id' : stock.location.id,
+                'location_name' : stock.location.name,
+                'store_name' : product.store.name,
+                'price' : round(stock.price, 2),
+                'final_price' : round(stock.final_price, 2),
+                'discount' : stock.discount,
+                'image' : product.cover_image,
+                'quantity' : quantity,
+            })
+
+    grand_total = (subtotal - discount_applied) + platform_fee + delivery_charges
+    context = {
+        'data' : data,
+        'subtotal' : round(subtotal, 2),
+        'discount_applied' : round(discount_applied, 2),
+        'platform_fee' : platform_fee,
+        'delivery_charges' : round(delivery_charges, 2),
+        'grand_total' : round(grand_total, 2)
+    }
+
+    response = render(request, 'Pharmacy/pharmacy_cart.html', context)
+    return render(request, 'Pharmacy/pharmacy_cart.html', context)
