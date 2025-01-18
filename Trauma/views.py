137e7491b6fa81f765539fb809051010d8c3eafa
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2025-01-18 12:54:05 +0500

    Auto commit at 2025-01-18 12:54:05

--- Trauma/views.py
+++ Trauma/views.py
@@ -19,9 +19,9 @@ from Profile.models import Profile
 from datetime import datetime
 from Product.models import Product
 
-# from django.views.decorators.cache import cache_page
+from django.views.decorators.cache import cache_page
 
-# @cache_page(60 * 15)
+@cache_page(60 * 15)
 def homePage(request):
     context = {}
 
