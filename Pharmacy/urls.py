b0ebdba4976d55259b79eaeffb2b9a7a9469f9bd
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2025-01-19 14:44:54 +0500

    Auto commit at 2025-01-19 14:44:54

--- Pharmacy/urls.py
+++ Pharmacy/urls.py
@@ -3,10 +3,12 @@ from django.urls import path
 
 
 from . import views as pharmacy_views
+from django.contrib.auth.decorators import login_required
+
 
 urlpatterns = [
     path('', pharmacy_views.PharmacyLandingPage, name='PharmacyLandingPage'),
     path('search/', pharmacy_views.PharmacySearchPage, name='PharmacySearchPage'),
     path('cart/', pharmacy_views.PharmacyCartPage, name='PharmacyCartPage'),
-    path('cart/checkout/', pharmacy_views.PharmacyCartCheckoutPage, name='PharmacyCartCheckoutPage'),
+    path('cart/checkout/', login_required(pharmacy_views.PharmacyCartCheckoutPage, login_url='/auth/login/'), name='PharmacyCartCheckoutPage'),
 ] 
