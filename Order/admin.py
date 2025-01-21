6e6158601e23324da2f5d936234103ecf0963ff5
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2025-01-21 21:33:18 +0500

    Auto commit at 2025-01-21 21:33:18

--- Order/admin.py
+++ Order/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Order, OrderItem
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = [
+        'user',
+        "subtotal",
+        "discount",
+        "platform_fee",
+        "delivery_charges",
+        "total_amount",
+        "payment_method",
+        "payment_status",
+        "created_at",
+    ]
