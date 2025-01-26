134a5804ee02c09d98d7d59bf1aaf39384247361
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2025-01-26 11:01:01 +0500

    Auto commit at 2025-01-26 11:01:01

--- Constants/Emails/OrderEmail.py
+++ Constants/Emails/OrderEmail.py
@@ -11,20 +11,19 @@ def sendNewOrderEmailToAdmin(order_instance, email_log_instance=None):
         order_items.append(f'\t{itm.product.name} \t\t {itm.final_price}x{itm.quantity}')
     
     subject = 'TraumaCare : New Order'
-    # message = f"""
-    #         New Order.\n
-    #         Order ID : #{order_instance.id}\n
-    #         Date : #{str(order_instance.created_at)}\n
-    #         User : {order_instance.user.full_name}\n
-    #         subtotal : {order_instance.subtotal}\n
-    #         discount : {order_instance.discount}\n
-    #         platform_fee : {order_instance.platform_fee}\n
-    #         delivery_charges : {order_instance.delivery_charges}\n
-    #         total_amount : {order_instance.total_amount}\n
-    #         Order Items : {order_instance.order_items.all().count()}\n
-    #         {"\n".join(order_items)}\n
-    #     """
-    message = ''
+    message = f"""
+            New Order.\n
+            Order ID : #{order_instance.id}\n
+            Date : #{str(order_instance.created_at)}\n
+            User : {order_instance.user.full_name}\n
+            subtotal : {order_instance.subtotal}\n
+            discount : {order_instance.discount}\n
+            platform_fee : {order_instance.platform_fee}\n
+            delivery_charges : {order_instance.delivery_charges}\n
+            total_amount : {order_instance.total_amount}\n
+            Order Items : {order_instance.order_items.all().count()}\n
+            {"\n".join(order_items)}\n
+        """
     recievers = [settings.EMAIL_HOST_USER, 'huzaifa.officialmail@gmail.com']
 
     if email_log_instance:
