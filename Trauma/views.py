e5ed69aee0ebab03537bce382cd00312604d0021
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2025-01-28 23:06:47 +0500

    Auto commit at 2025-01-28 23:06:47

--- Trauma/views.py
+++ Trauma/views.py
@@ -42,14 +42,15 @@ def shortCodeRedirect(request, short_code_id):
 def homePage(request):
     context = {}
 
-    doctors = Doctor.objects.filter(
-        is_active = True,
-        is_deleted = False,
-        is_blocked = False,
-    ).prefetch_related(
+    doctors = Doctor.objects.prefetch_related(
         'doctor_medias',
         'doctor_reviews',
         'doctor_available_days',
+    ).filter(
+        is_active = True,
+        is_deleted = False,
+        is_blocked = False,
+        doctor_timeslots__isnull=False
     ).order_by('?')
 
     context['doctors'] = doctors[:8]
