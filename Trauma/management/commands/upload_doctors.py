682a5e6ff362a2dbcef943602e8a48ae3d6b997a
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2025-01-28 07:09:39 +0500

    Auto commit at 2025-01-28 07:09:39

--- Trauma/management/commands/upload_doctors.py
+++ Trauma/management/commands/upload_doctors.py
@@ -95,9 +95,12 @@ class Command(BaseCommand):
         with open('Files/uniqueDoctors.json' , 'r') as input_file:
             reader = json.load(input_file)
             for doctor_id, doctor_obj in reader.items():
-                if counter <= 5844:
-                    counter += 1
-                    continue
+                specialities = doctor_obj.get('MainCategory', '').split(',')
+                print(specialities)
+                # if counter <= 5844:
+                #     counter += 1
+                #     continue
+                continue
                 # print(json.dumps(doctor_obj))
                 # if doctor_obj['available_days'] == '':
                 #     print('Skipping', doctor_id)
