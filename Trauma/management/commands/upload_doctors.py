02042d557f7e71b5b16f11f563c133df0932fb36
HuzaifaYaqoob <redexpo.officialmail@gmail.com>
2025-01-28 07:10:37 +0500

    Auto commit at 2025-01-28 07:10:37

--- Trauma/management/commands/upload_doctors.py
+++ Trauma/management/commands/upload_doctors.py
@@ -95,7 +95,7 @@ class Command(BaseCommand):
         with open('Files/uniqueDoctors.json' , 'r') as input_file:
             reader = json.load(input_file)
             for doctor_id, doctor_obj in reader.items():
-                specialities = doctor_obj.get('MainCategory', '').split(',')
+                specialities = doctor_obj.get('MainCategory', '')
                 print(specialities)
                 # if counter <= 5844:
                 #     counter += 1
