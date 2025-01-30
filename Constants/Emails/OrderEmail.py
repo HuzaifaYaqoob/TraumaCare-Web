d8253e6562021a076c33e0a9647337c9fffdb585
danishyaqoobofficial <danishyaqoob117@gmail.com>
2025-01-30 11:26:16 +0500

    cjanging

--- Constants/Emails/OrderEmail.py
+++ Constants/Emails/OrderEmail.py
@@ -1,5 +1,3 @@
-
-
 from django.core.mail import send_mail, EmailMultiAlternatives
 from django.template.loader import get_template
 from django.conf import settings
@@ -11,10 +9,8 @@ def sendNewOrderEmailToAdmin(order_instance, email_log_instance=None):
         order_items.append(f'\t{itm.product.name} \t\t {itm.final_price}x{itm.quantity}')
     
     subject = 'TraumaCare : New Order'
-    try:
-        message = f'New Order.\n Order ID : #{order_instance.id}\n Date : #{str(order_instance.created_at)}\nUser : {order_instance.user.full_name}\n Delivery At : {order_instance.shipping.address}\nsubtotal : {order_instance.subtotal}\ndiscount : {order_instance.discount}\nplatform_fee : {order_instance.platform_fee}\ndelivery_charges : {order_instance.delivery_charges}\ntotal_amount : {order_instance.total_amount}\nOrder Items : {order_instance.order_items.all().count()}\n{"\n".join(order_items)}\n'
-    except:
-        message = f'New Order Order ID : #{order_instance.id}\n Date : #{str(order_instance.created_at)}\n '
+    # message = f'New Order.\n Order ID : #{order_instance.id}\n Date : #{str(order_instance.created_at)}\nUser : {order_instance.user.full_name}\n Delivery At : {order_instance.shipping.address}\nsubtotal : {order_instance.subtotal}\ndiscount : {order_instance.discount}\nplatform_fee : {order_instance.platform_fee}\ndelivery_charges : {order_instance.delivery_charges}\ntotal_amount : {order_instance.total_amount}\nOrder Items : {order_instance.order_items.all().count()}\n{"\n".join(order_items)}\n'
+    message = 'New Order'
     recievers = [settings.EMAIL_HOST_USER, 'huzaifa.officialmail@gmail.com']
 
     if email_log_instance:
