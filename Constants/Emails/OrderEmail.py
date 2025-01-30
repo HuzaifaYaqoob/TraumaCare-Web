a7c40d7f31c470e26013b9a184414a9eae636829
danishyaqoobofficial <danishyaqoob117@gmail.com>
2025-01-30 13:52:34 +0500

    privacy page completed

--- Constants/Emails/OrderEmail.py
+++ Constants/Emails/OrderEmail.py
@@ -9,8 +9,8 @@ def sendNewOrderEmailToAdmin(order_instance, email_log_instance=None):
         order_items.append(f'\t{itm.product.name} \t\t {itm.final_price}x{itm.quantity}')
     
     subject = 'TraumaCare : New Order'
-    # message = f'New Order.\n Order ID : #{order_instance.id}\n Date : #{str(order_instance.created_at)}\nUser : {order_instance.user.full_name}\n Delivery At : {order_instance.shipping.address}\nsubtotal : {order_instance.subtotal}\ndiscount : {order_instance.discount}\nplatform_fee : {order_instance.platform_fee}\ndelivery_charges : {order_instance.delivery_charges}\ntotal_amount : {order_instance.total_amount}\nOrder Items : {order_instance.order_items.all().count()}\n{"\n".join(order_items)}\n'
-    message = 'New Order'
+    message = f'New Order.\n Order ID : #{order_instance.id}\n Date : #{str(order_instance.created_at)}\nUser : {order_instance.user.full_name}\n Delivery At : {order_instance.shipping.address}\nsubtotal : {order_instance.subtotal}\ndiscount : {order_instance.discount}\nplatform_fee : {order_instance.platform_fee}\ndelivery_charges : {order_instance.delivery_charges}\ntotal_amount : {order_instance.total_amount}\nOrder Items : {order_instance.order_items.all().count()}\n{"--------".join(order_items)}\n'
+    # message = 'New Order'
     recievers = [settings.EMAIL_HOST_USER, 'huzaifa.officialmail@gmail.com']
 
     if email_log_instance:
